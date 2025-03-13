33b5bbc9e4259cd2b2635745ba2fac295c1905b9
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-14 00:12:41 +0200

    added tailwind dep

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../App.css"
 
-function Register() {
+const Register = () => {
   return (
-    <>
-      <h1 className="nigaa">Register</h1>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold text-gray-800">Register Form</h1>
+    </div>
   );
-}
+};
 
 export default Register;
