0b9a021260cc133cea57077cd7c9ca8b4fa64748
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-14 15:29:17 +0200

    removed tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,50 +3,7 @@ import "../App.css";
 
 const Login = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-base-200">
-      <div className="w-full max-w-md bg-base-100 p-8 rounded-lg shadow-xl border border-base-300">
-        <h1 className="text-3xl font-semibold text-center mb-6 text-primary">
-          Log in
-        </h1>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-base-content"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className="input input-bordered w-full mt-2"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-base-content"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              className="input input-bordered w-full mt-2"
-            />
-          </div>
-
-          <div className="mt-6">
-            <button type="submit" className="btn btn-primary w-full">
-              Login
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <h1>Login</h1>
   );
 };
 
