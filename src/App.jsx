306d66fe9dbe4eb8bbda6c64d294771302f1cc7f
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-17 04:27:21 +0200

    /store path

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import "./App.css"
 
 function App() {
   return (
-    <Router>
+    <Router basename="/store">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
