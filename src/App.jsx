c825e46737a85a247d284a5aca1a53af9645ef6f
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-04-05 23:35:33 +0300

    lenis slower

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
     });
