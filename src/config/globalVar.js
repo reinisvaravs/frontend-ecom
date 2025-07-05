59715ea6b5cd975773d01b4bbb26182e1480eda4
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-07-05 17:21:01 +0300

    fix

--- src/config/globalVar.js
+++ src/config/globalVar.js
@@ -1,4 +1,3 @@
-// const BACKEND_URL = "https://backend-ecom-j57d.onrender.com"; // no slash in the end!
-const BACKEND_URL = "localhost:8383";
+const BACKEND_URL = "https://backend-ecom-j57d.onrender.com"; // no slash in the end!
 
 export default BACKEND_URL;
